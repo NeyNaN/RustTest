6b226e64f61fcfd14f0e5fedf5cde1b1e4b9dab3
Kevin Anderton <kanderton@g.harvard.edu>
2022-07-28 18:00:44 -0400

    First commit

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let text_var = "variable";
+    println!("Hello, world!");
+    println!(
+        "Testing formatting of {} {} {}",
+        "formatted", text_var, "inputs"
+    )
+}
