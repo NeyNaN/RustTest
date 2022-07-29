6b0ec7829fa11849a582f084096418097db97761
Kevin Anderton <kanderton@g.harvard.edu>
2022-07-29 16:32:55 -0400

    Encapsulate greeting

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-fn main() {
+fn greetings() {
     let text_var = "variable";
     println!("Hello, world!");
     println!(
         "Testing formatting of {} {} {}",
         "formatted", text_var, "inputs"
     )
+}
+
+fn main() {
+    greetings();
 }
