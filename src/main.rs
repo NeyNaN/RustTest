160f9f5bbe4c0c8afbdb81e4b5ea25e230f11c82
Kevin Anderton <kanderton@g.harvard.edu>
2022-07-28 20:02:14 -0400

    Added simple guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,57 @@
-fn main() {
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn greetings() {
     let text_var = "variable";
     println!("Hello, world!");
     println!(
         "Testing formatting of {} {} {}",
         "formatted", text_var, "inputs"
     )
+}
+
+fn guessing() {
+    
+    let target = rand::thread_rng().gen_range(1..=100);
+    
+    // println!("Target is {target}");
+
+    println!("Guess the number!");
+    println!();
+
+    let mut i: u32 = 0;
+    loop {
+        i += 1;
+        let mut guess = String::new();
+
+        println!("Enter guess: ");
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid guess");
+                continue;
+            }
+        };
+        
+        println!("You guessed {guess}");
+    
+        match guess.cmp(&target) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("Correct! {i} guesses used!");
+                break;
+            }
+        }
+    }
+}
+
+fn main() {
+    greetings();
+    guessing();
 }
